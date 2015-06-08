bbcec7f217767781e938222332bf34cd39697190
Anna <anna.klimas@gmail.com>
2015-06-08 16:07:50 +0100

    first commit - went through all pills in further javascript up to and inc grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+    },
+    watch: {
+      files: ['src/**/*.js','spec/**/*.js'],
+      tasks: ['jshint']
+    },
+    jasmine: {
+      pivotal: {
+        src: 'src/**/*.js',
+        options: {
+          specs: 'spec/*Spec.js',
+          helpers: 'spec/*Helper.js'
+        }
+      }
+
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['watch']);
+};
