957cdf44cb32cacecd2c29346d61de6412c52775
Anna <anna.klimas@gmail.com>
2015-06-08 16:42:02 +0100

    added watch to gruntfile, so jshint is run every time spec or src file is amended and saved

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'src/**/*.js', 'spec/**/*.js']
+    },
+    watch: {
+      files: ['src/**/*.js', 'spec/**/*.js'],
+      tasks: ['jshint']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint']);
 
